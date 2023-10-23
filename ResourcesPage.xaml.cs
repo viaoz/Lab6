3d432cf489584f8749245bfd5a2998c00a643927
mise11 <115748357+mise11@users.noreply.github.com>
2023-10-23 17:26:13 -0500

    Add files via upload

--- ResourcesPage.xaml.cs
+++ ResourcesPage.xaml.cs
@@ -1,9 +1,25 @@
+using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
 namespace Lab6_Starter;
-
 public partial class ResourcesPage : ContentPage
 {
 	public ResourcesPage()
 	{
-        InitializeComponent();
-	}
+  
+
+            InitializeComponent();
+
+            // Create a list of URLs (or resources)
+            List<string> urlList = new List<string>
+            {
+                "https://www.AirportExample1.com",
+                "https://www.AirportExample2.com",
+                // Add more URLs or resources here
+            };
+
+            // Set the ItemsSource of the CollectionView to the list of URLs
+            collectionView.ItemsSource = urlList;
+        
+    }
 }
