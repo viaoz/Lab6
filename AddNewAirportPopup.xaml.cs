a96b1c64751bf6b227e90d343f19cd82ea70cc4c
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-23 11:35:26 -0500

    Setup the popup. Added community toolkit to MauProgram.cs.

---
+++ AddNewAirportPopup.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace Lab6_Solution;
+
+public partial class AddNewAirportPopup : Popup
+{
+	public AddNewAirportPopup()
+	{
+		InitializeComponent();
+	}
+
+	public void AddAirportButtonClicked(Object sender, EventArgs args)
+	{
+		/* Code here */
+	}
+
+	public void ClosePopupButtonClicked(Object sender, EventArgs args)
+	{
+		/* Code here */
+	}
+}
