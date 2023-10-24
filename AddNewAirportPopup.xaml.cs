4905f6b6530d3ff72f136a10510acc1672bef4fd
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-10-23 22:02:08 -0500

    New code comments.

--- AddNewAirportPopup.xaml.cs
+++ AddNewAirportPopup.xaml.cs
@@ -2,18 +2,34 @@
 
 namespace Lab6_Solution;
 
+/// <summary>
+/// Authors: Evan Olson, Olivia Ozbaki, Alex Ceithamer
+/// </summary>
 public partial class AddNewAirportPopup : Popup
 {
+	/// <summary>
+	/// Initializes the popup component.
+	/// </summary>
 	public AddNewAirportPopup()
 	{
 		InitializeComponent();
 	}
 
+	/// <summary>
+	/// Adds an airport with the specified information to the database.
+	/// </summary>
+	/// <param name="sender">Sender</param>
+	/// <param name="args">Arguments</param>
 	public void AddAirportButtonClicked(System.Object sender, System.EventArgs args)
 	{
-		/* Code here */
+		/* Code here TO BE IMPLEMENTED */
 	}
 
+	/// <summary>
+	/// Closes the "Add New Airport" popup.
+	/// </summary>
+	/// <param name="sender">Sender</param>
+	/// <param name="args">Arguments</param>
 	public void ClosePopupButtonClicked(System.Object sender, System.EventArgs args)
 	{
 		Close();
