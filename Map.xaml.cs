c4ed5cc882feb641cbe9e10214c9c11bcb1cfc5d
olsone50 <olsoneva000@gmail.com>
2023-11-06 10:14:25 -0600

    Added Stubbed Methods For Displaying Airports

--- Map.xaml.cs
+++ Map.xaml.cs
@@ -1,3 +1,4 @@
+
 namespace Lab6_Starter; 
 public partial class Map : ContentPage
 {
@@ -5,4 +6,37 @@ public partial class Map : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
+    /// <summary>
+    /// Calls DisplayAllAirports to display a pin for every airport when map is established
+    /// </summary>
+    protected override void OnAppearing()
+    {
+        DisplayAllAirports();
+    }
+    /// <summary>
+    /// Calls BusinessLogic to get all airports and displays every airport as a pin on map
+    /// </summary>
+    private void DisplayAllAirports()
+	{
+		//Might Call Details Popup on Click of Pin
+	}
+
+    /// <summary>
+    /// Calls BusinessLogic to get airports that are not visited and displays the airports as a pin on map
+    /// </summary>
+    private void DisplayUnvisitedAirports()
+	{
+
+	}
+
+    /// <summary>
+    /// Calls BusinessLogic to get airports that are visited and displays the airports as a pin on map
+    /// </summary>
+    private void DisplayVisitedAirports()
+	{
+
+	}
+
 }
