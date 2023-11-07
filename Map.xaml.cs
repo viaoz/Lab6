c193cbc6969431919ef775f2c535c3896e7d7278
Olivia Ozbaki <ozbako81@uwosh.edu>
2023-11-06 22:38:31 -0600

    Not entirely sure about this one.

--- Map.xaml.cs
+++ Map.xaml.cs
@@ -1,42 +1,16 @@
+using System.Collections.ObjectModel;
+using Lab6_Solution.Model;
+using Lab6_Starter.Model;
 
-namespace Lab6_Starter; 
+namespace Lab6_Starter;
+
 public partial class Map : ContentPage
 {
 	public Map()
 	{
 		InitializeComponent();
-	}
-
-
-    /// <summary>
-    /// Calls DisplayAllAirports to display a pin for every airport when map is established
-    /// </summary>
-    protected override void OnAppearing()
-    {
-        DisplayAllAirports();
+        BindingContext = MauiProgram.BusinessLogic;
     }
-    /// <summary>
-    /// Calls BusinessLogic to get all airports and displays every airport as a pin on map
-    /// </summary>
-    private void DisplayAllAirports()
-	{
-		//Might Call Details Popup on Click of Pin
-	}
 
-    /// <summary>
-    /// Calls BusinessLogic to get airports that are not visited and displays the airports as a pin on map
-    /// </summary>
-    private void DisplayUnvisitedAirports()
-	{
-
-	}
-
-    /// <summary>
-    /// Calls BusinessLogic to get airports that are visited and displays the airports as a pin on map
-    /// </summary>
-    private void DisplayVisitedAirports()
-	{
-
-	}
-
+    private readonly ObservableCollection<AirportPin> AirportPins = MauiProgram.BusinessLogic.AirportPins;
 }
--- Model/IBusinessLogic.cs
+++ Model/IBusinessLogic.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Lab6_Solution.Model;
 
 namespace Lab6_Starter.Model;
 public interface IBusinessLogic
 {
+    ObservableCollection<AirportPin> AirportPins { get; set; }
+
     AirportAdditionError AddAirport(String id, String city, DateTime dateVisited, int rating);
     AirportDeletionError DeleteAirport(String id);
     AirportEditError EditAirport(String id, String city, DateTime dateVisited, int rating);
     Airport FindAirport(String id);
     String CalculateStatistics();
     ObservableCollection<Airport> GetAllAirports();
-    ObservableCollection<Airport> GetUnvisitedAirports();
-    ObservableCollection<Airport> GetVisitedAirports();
-
+    ObservableCollection<Airport> GetWisconsinAirports();
 }
